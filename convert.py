0b280b6bd8cf531d2d4136a71f1d81b2fd95ec12
griffi-gh <45996170+griffi-gh@users.noreply.github.com>
2020-03-18 17:01:27 +0200

    owo

--- convert.py
+++ convert.py
@@ -10,18 +10,19 @@ chunk = anvil.Chunk.from_region(region, 0, 0)
 blocks=''
 
 it=0
+ip=0
 for ix in range(0,16):
     for iz in range(0,16):
         for iy in range(0,256):
             it+=1
             if it%2000==0:
                 print('converting... '+str(it)+'/'+str(16*256*16))
-            
             block = chunk.get_block(ix,iy,iz)
             if block.id != 'air':
+                ip+=1
                 blocks+=str(ix)+','+str(iy)+','+str(iz)+'\n'   
 
-print(it)
+print(str(ip)+' '+str(it))
 f = open("converted.txt", "w")
 f.write(blocks)
 f.close()
